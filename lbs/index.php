06fdb427e95649d34aa8690efafb139b09c7fafe
Lv Zhaobo <lvzhaobopku@163.com>
2014-03-18 16:55:58 +0800

    add icp number

--- lbs/index.php
+++ lbs/index.php
@@ -14,47 +14,25 @@
 <body>
     <div id="header">
 	  <div>
-	    <div class="page_title">
-		  <div class="title_style" style="float:left;">
-		    <div style="width:360px;border-right:1px solid #0099FF;float:left;">
-		      <div style="float:left;">
-			    <span style="font-size:28px;">家在台前，爱台前</span>
-			  </div>
-			  <div style="float:right;">
-				<span style="font-size:16px;color:#0099FF;">@台前网</span><br />
-			    <span style="font-size:10px;color:#0099FF;">TAIQIANWANG</span>
-			  </div>
-			</div>
-			<div style="margin:8px 0 0 0px;float:left;">
-		      <span style="font-size:22px;color:#0099FF;">一站式生活平台</span>
-			</div>
-			<div style="margin:12px 0 0 48px;float:left;font-size:14px;">
-		      <span style="color:#666666;">由 </span><span style="color:#FF9900;">梦想之星工作室</span><span style="color:#666666;"> 提供技术支持</span>
-			  <br />
-			  <span style="font-weight:normal;"><?php echo date("Y年m月d日 H:i:s",time())?></span><br />
-			  <div style="margin-left:-28px;"><script type="text/javascript" src="http://ext.weather.com.cn/66786.js"></script></div>
+	    <?php include "../src/_header.php";?>
+		<div id="nav">
+			<div id="nav_content">
+			  <ul>
+				<li><a href="../index.php" title="">首页</a></li>
+				<li><a href="../items.php" title="">入驻客户</a></li>
+				<li class="seleted"><a href="index.php" title="">地图搜索</a></li>
+				<li><a href="#" title="">业务合作</a></li>
+				<li><a href="../about.php" title="">关于我们</a></li>
+				<li><a href="../contect.php" title="">联系我们</a></li>
+			  </ul>
 			</div>
 		  </div>
 		</div>
-	  </div>
-	</div>
-	<div id="nav">
-	    <div id="nav_content">
-		  <ul>
-		    <li><a href="../index.php" title="">首页</a></li>
-		    <li><a href="../items.php" title="">入驻客户</a></li>
-			<li class="seleted"><a href="index.php" title="">地图搜索</a></li>
-			<li><a href="#" title="">业务合作</a></li>
-			<li><a href="../about.php" title="">关于我们</a></li>
-			<li><a href="../contect.php" title="">联系我们</a></li>
-		  </ul>
-		</div>
-	  </div>
-	  <div class="clearfloat"></div>
-	  <div id="content">
-	  <div id="main">
-    <div id="pageMiddle">
-        <div class="container" id = "header">
+		<div class="clearfloat"></div>
+		<div id="content">
+		  <div id="main">
+		  <div id="pageMiddle">
+			<div class="container" id = "header">
             <div id="filterBox">
             </div>
             <div id="toolsBar" class="clearfix">
--- src/_footer.php
+++ src/_footer.php
@@ -2,7 +2,7 @@
 	echo <<<EOF
 			<div id="footer">
 			  <div style="margin-top:20px;text-align:center;font-size:12px;font-weight:bold;line-height:24px;">
-				<a href="about.php" style="">技术支持@梦想之星工作室</a><br />
+				<a href="about.php" style="color:#2D374B;">版权所有@2014 梦想之星工作室</a>&nbsp;&nbsp;<a href="" style="color:#2D374B;">沪ICP备14009817号</a><br />
 				<span style="">建议使用IE8及以上、火狐、Chrome等浏览器浏览</span><br />
 				<div>
 				  <span>使用百度统计</span>
